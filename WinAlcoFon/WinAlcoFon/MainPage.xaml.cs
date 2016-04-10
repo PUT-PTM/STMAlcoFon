31b16418ef941ae4ad89851295b0f4506698af94
Tomasz Braczyński <tomekbraczynski@o2.pl>
2016-04-10 10:57:21 +0200

    Dalsza rozbudowa UI
    Poprawiony wykres (stała oś Y, usunięta legenda). Dodano przycisk do
    rozpoczęcia pomiaru oraz kalibracji. Dodano progres bar - używany
    podczas pomiarów. Dodano przycisk połącz oraz listę rozwijaną - do
    wyboru urządzenia BT i połączenia się z nim.

--- WinAlcoFon/WinAlcoFon/MainPage.xaml.cs
+++ WinAlcoFon/WinAlcoFon/MainPage.xaml.cs
@@ -15,18 +15,17 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using WinRTXamlToolkit.Controls.DataVisualization.Charting;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Rfcomm;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Enumeration.Pnp;
 namespace WinAlcoFon
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
-        int temp = 0;
-        bool dziala;
+        int temp = 0;//tylko do rand przydatny
+        bool uruchomione;
+        private SemaphoreSlim signal = new SemaphoreSlim(0, 1);
         public class dane_do_wykresu
         {
             public int id { get; set; }
@@ -36,28 +35,65 @@ namespace WinAlcoFon
         public MainPage()
         {
             this.InitializeComponent();
+            ((LineSeries)LineChart.Series[0]).DependentRangeAxis = new LinearAxis()
+            {
+                Maximum = 200,
+                Minimum = 0,
+                Orientation = AxisOrientation.Y,
+                ShowGridLines = true
+            };
             this.zaladuj_wykres();
-            dziala = false;
-            Zatrzymaj.IsEnabled = false;
+            uruchomione = false;
+            blokuj_przyciski(true);
+            Stop.IsEnabled = false;
+            szukaj_bt();
+        }
+        async void szukaj_bt()
+        {
+            DeviceInformationCollection urzadzenia_bt;
+            urzadzenia_bt = await DeviceInformation.FindAllAsync(BluetoothDevice.GetDeviceSelector());
+            if (urzadzenia_bt.Count > 0)
+            {
+                List<string> items = new List<string>();
+                foreach (var chatServiceInfo in urzadzenia_bt)
+                {
+                    items.Add(chatServiceInfo.Name);
+                }
+                Do_polaczenia.ItemsSource = items;
+            }
+            else
+            {
+                List<string> items = new List<string>();
+                items.Add("pusto");
+                Do_polaczenia.ItemsSource = items;
+            }
+        }
+        void blokuj_przyciski(bool zablokowane)
+        {
+            if(zablokowane)
+            {
+                Start.IsEnabled = false;
+                Stop.IsEnabled = false;
+                Pomiar.IsEnabled = false;
+                Kalibruj.IsEnabled = false;
+                uruchomione = true;
+            }
+            else
+            {
+                Start.IsEnabled = true;
+                Stop.IsEnabled = false;//domyslnie wylaczony
+                Pomiar.IsEnabled = true;
+                Kalibruj.IsEnabled = true;
+                uruchomione = false;
+            }
         }
         void zaladuj_wykres()
         {
             Random rand = new Random();
-            lista_danych.Add(new dane_do_wykresu() { id = temp++, wartosc = 0 });
-            lista_danych.Add(new dane_do_wykresu() { id = temp++, wartosc = 0 });
-            lista_danych.Add(new dane_do_wykresu() { id = temp++, wartosc = 0 });
-            lista_danych.Add(new dane_do_wykresu() { id = temp++, wartosc = 0 });
-            lista_danych.Add(new dane_do_wykresu() { id = temp++, wartosc = 0 });
-            lista_danych.Add(new dane_do_wykresu() { id = temp++, wartosc = 0 });
-            lista_danych.Add(new dane_do_wykresu() { id = temp++, wartosc = 0 });
-            lista_danych.Add(new dane_do_wykresu() { id = temp++, wartosc = 0 });
-            lista_danych.Add(new dane_do_wykresu() { id = temp++, wartosc = 0 });
-            lista_danych.Add(new dane_do_wykresu() { id = temp++, wartosc = 0 });
-            lista_danych.Add(new dane_do_wykresu() { id = temp++, wartosc = 0 });
-            lista_danych.Add(new dane_do_wykresu() { id = temp++, wartosc = 0 });
-            lista_danych.Add(new dane_do_wykresu() { id = temp++, wartosc = 0 });
-            lista_danych.Add(new dane_do_wykresu() { id = temp++, wartosc = 0 });
-            lista_danych.Add(new dane_do_wykresu() { id = temp++, wartosc = 0 });
+            for(int i=0;i<15;i++)
+            {
+                lista_danych.Add(new dane_do_wykresu() { id = temp++, wartosc = 0 });
+            }
             (LineChart.Series[0] as LineSeries).ItemsSource = lista_danych;
         }
         private void odswiez_wykres()
@@ -67,23 +103,74 @@ namespace WinAlcoFon
             lista_danych.Add(new dane_do_wykresu() { id = temp++, wartosc = rand.Next(0, 200) });
             (LineChart.Series[0] as LineSeries).Refresh();
         }
-        private async void Odswiez_Click(object sender, RoutedEventArgs e)
+        private async void Start_Click(object sender, RoutedEventArgs e)
         {
-            Odswiez.IsEnabled = false;
-            Zatrzymaj.IsEnabled = true;
-            dziala = true;
-            while (dziala)
+            blokuj_przyciski(true);
+            Stop.IsEnabled = true;
+            while (uruchomione)
             {
                 odswiez_wykres();
                 await Task.Delay(250);
             }
         }
-
-        private void Zatrzymaj_Click(object sender, RoutedEventArgs e)
+        private void Stop_Click(object sender, RoutedEventArgs e)
         {
-            Odswiez.IsEnabled = true;
-            Zatrzymaj.IsEnabled = false;
-            dziala = false;
+            blokuj_przyciski(false);
+        }
+        private async void Kalibruj_Click(object sender, RoutedEventArgs e)
+        {
+            //kilka odczytów na suchym powietrzu i usrednienie wyniku
+            //wynik odjac od maksimum jakie moze odczytac wynik i podzielic 
+            //pomiary otrzymywane przez BT mnozyc przez ostateczny wynik by otrzymać ‰
+            blokuj_przyciski(true);
+            Wynik_pomocniczy.Text = "Pozostało";
+            pomiar();
+            await signal.WaitAsync();
+            Wynik_pomocniczy.Text = "Czujnik został";
+            Wynik.Text = "skalibrowany";
+            blokuj_przyciski(false);
+            Kalibruj.Content = "Czujnik skalibrowany";
+        }
+        private async void Pomiar_Click(object sender, RoutedEventArgs e)
+        {
+            blokuj_przyciski(true);
+            Wynik_pomocniczy.Text = "Weź głęboki wdech";
+            for (int i = 3; i>=1; i--)
+            {
+                Wynik.Text = "pomiar za " +i + " sekund";
+                await Task.Delay(1000);
+            }
+            Progres.Value = 0;
+            Wynik_pomocniczy.Text = "Dmuchaj przez";
+            pomiar();
+            await signal.WaitAsync();
+            Wynik_pomocniczy.Text = "Twój wynik to";
+            Wynik.Text = "X ‰";
+            blokuj_przyciski(false);
+        }
+        async void pomiar()
+        {
+            int ilosc_probek=20;
+            Progres.Value = 0;
+            for (int i=0;i< ilosc_probek; i++)
+            {
+                Wynik.Text =(ilosc_probek/4-i/4)+" sekund";
+                Progres.Value += (float)100/ilosc_probek;
+                await Task.Delay(250);
+            }
+            signal.Release();
+        }
+        private void Polacz_Click(object sender, RoutedEventArgs e)
+        {
+            blokuj_przyciski(false);
+            Polacz.Content = "Połączono";
+            Do_polaczenia.IsEnabled = false;
+            Polacz.IsEnabled = false;
+            Odswiez.IsEnabled = false;
+        }
+        private void Odswiez_Click(object sender, RoutedEventArgs e)
+        {
+            szukaj_bt();
         }
     }
 }
