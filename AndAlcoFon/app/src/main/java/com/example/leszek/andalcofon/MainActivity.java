d6d50bf7e65d0eda7dc8fe0a3f4e9c5e47fc8f48
Leszek Stencel <stencelleszek@gmail.com>
2016-04-11 12:42:25 +0200

    Utworzenie aplikacji + wykres

---
+++ AndAlcoFon/app/src/main/java/com/example/leszek/andalcofon/MainActivity.java
@@ -0,0 +1,169 @@
+package com.example.leszek.andalcofon;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.RelativeLayout;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout mainLayout;
+    private LineChart mChart;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mainLayout = (RelativeLayout) findViewById(R.id.mainLayout);
+        // create line chart
+        mChart = new LineChart(this);
+        // add to layout
+        mainLayout.addView(mChart);
+
+        // customize line chart
+        mChart.setDescription("");
+        mChart.setNoDataTextDescription("No data for the moment");
+
+        // enable value
+        mChart.setHighlightEnabled(true);
+
+        // enable touch gestures
+        mChart.setTouchEnabled(true);
+
+        mChart.setDragEnabled(true);
+        mChart.setScaleEnabled(true);
+        mChart.setDrawGridBackground(false);
+
+        //enable pinch zoom t oavoid scaling x and y axis separately
+        mChart.setPinchZoom(true);
+
+        // alternative background color
+        mChart.setBackgroundColor(Color.rgb(255,140,0));
+
+        //now we work with data
+        LineData data= new LineData();
+        data.setValueTextColor(Color.WHITE);
+
+        // add data to line chart
+        mChart.setData(data);
+
+        //get legend object
+        Legend l= mChart.getLegend();
+        //Customize legend
+        l.setForm(Legend.LegendForm.LINE);
+        l.setTextColor(Color.WHITE);
+
+        XAxis x1 = mChart.getXAxis();
+        x1.setTextColor(Color.WHITE);
+        x1.setDrawGridLines(false);
+        x1.setAvoidFirstLastClipping(true);
+
+        YAxis y1 = mChart.getAxisLeft();
+        y1.setTextColor(Color.WHITE);
+        y1.setAxisMaxValue(140f);
+        y1.setDrawGridLines(true);
+
+        YAxis y12 = mChart.getAxisRight();
+        y12.setEnabled(false);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+                for(int i=0;i<100;i++)
+                {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            addEntry();
+                        }
+                    });
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void addEntry()
+    {
+        LineData data = mChart.getData();
+        if (data!=null)
+        {
+            LineDataSet set=data.getDataSetByIndex(0);
+            if(set==null)
+            {
+                set= createSet();
+                data.addDataSet(set);
+            }
+            // add a new random value
+            data.addXValue("");
+            data.addEntry(new Entry((float) (Math.random() * 75) + 60f, set.getEntryCount()), 0);
+
+            mChart.notifyDataSetChanged();
+            mChart.setVisibleXRange(6);
+            mChart.moveViewToX(data.getXValCount()-7);
+
+        }
+    }
+    //method to create set
+    private LineDataSet createSet()
+    {
+        LineDataSet set = new LineDataSet(null,"Aktualna wartosc");
+        set.setDrawCubic(true);
+        set.setCubicIntensity(0.2f);
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setColor(ColorTemplate.getHoloBlue());
+        set.setCircleColor(ColorTemplate.getHoloBlue());
+        set.setLineWidth(2f);
+        set.setCircleSize(4f);
+        set.setFillAlpha(65);
+        set.setFillColor(ColorTemplate.getHoloBlue());
+        set.setHighLightColor(Color.rgb(244, 117, 177));
+        set.setValueTextColor(Color.WHITE);
+        set.setValueTextSize(10f);
+
+
+        return set;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
