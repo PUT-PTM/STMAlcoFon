f0717bed4fbe3fb9715374589860deff62c6802b
Tomasz Braczyński <tomekbraczynski@o2.pl>
2016-04-24 13:58:15 +0200

    Drobne zmiany wyglądu oraz poprawki stabilności i błędów
    - Przeskalowano wykres, teraz dane wyświetlane na wykresie są już
    przeliczone na promile
    - Usprawniono wyświetlanie danych
    - Dodano obsługę dotknięcia paska postępu, który wyświetli uwagi i
    informacje odnośnie programu
    - Poprawiono łączność Bluetooth, od teraz cyklicznie jest czyszczony
    bufor by zapewnić odczyt najświeższych danych (wcześniej bufor potrafił
    się przepełnić co skutkowało opóźnieniami)

--- WinAlcoFon/WinAlcoFon/MainPage.xaml.cs
+++ WinAlcoFon/WinAlcoFon/MainPage.xaml.cs
@@ -29,12 +29,12 @@ namespace WinAlcoFon
         private class dane_do_wykresu
         {
             public double id { get; set; }
-            public int wartosc { get; set; }
+            public double wartosc { get; set; }
         }
-        double temp = 0;//tylko do rand przydatny
+        double temp = 0;//do wartosci wyswietlanych pod osia OX
         bool uruchomione;
-        double wynik_w_promilach;
-        double sredni_wynik;
+        double wynik_w_promilach=0;
+        double usredniony_wynik = 0;
         double prog_0_promili = 150;//zmieniany podczas kalibracji
         double przelicznik = 0.0021287379624937;//zmieniany podczas kalibracji, poczatkowo obliczony dla progu=150, wyliczany z wzoru 8.4/(4096-prog_0_promili)
         //gdzie 8.4 promila to maksymalny odczyt z czujnika MQ-3 według specyfikacji
@@ -51,25 +51,41 @@ namespace WinAlcoFon
             this.InitializeComponent();
             ((LineSeries)LineChart.Series[0]).DependentRangeAxis = new LinearAxis()
             {
-                //Maximum = 200,
-                Minimum = 0,
+                //Maximum = 0.5,
+                //Minimum = -0.5,
                 Orientation = AxisOrientation.Y,
                 ShowGridLines = true
             };
-            this.zaladuj_wykres();
+            zaladuj_wykres();
             uruchomione = false;
             blokuj_przyciski(true);
             Stop.IsEnabled = false;
             szukaj_bt();
         }
-        void podglad()
+        void wypisz(string gora, string dol= "AlcoFon®")
         {
-            //odczyty.Text = "ostatni pomiar: " + pomiar.ToString() + '\n' + "prog 0: " + prog_0_promili + '\n' + "przelicznik: " + przelicznik;
+            if(gora!="")
+            {
+                tekst_pomocniczy.Text = gora;
+            }
+            if(dol!="")
+            {
+                Wynik.Text = dol;
+            }
         }
         private async Task<int> pojedynczy_odczyt()
         {
             byte[] b = new byte[4];
-            await ReaderData.LoadAsync(4);
+            try
+            {
+                await _socket.CancelIOAsync();//wyczyszczenie bufora
+                await ReaderData.LoadAsync(4);
+            }
+            catch
+            {
+                wypisz("Wystąpił błąd w transmisji danych, wykonaj pomiar jeszce raz","!BŁĄD!");
+                return 0;
+            }
             b[0] = ReaderData.ReadByte();
             b[1] = ReaderData.ReadByte();
             b[2] = ReaderData.ReadByte();
@@ -87,14 +103,14 @@ namespace WinAlcoFon
                 foreach (var chatServiceInfo in urzadzenia_bt)
                 {
                     items.Add(chatServiceInfo.Name);
-                    Wynik_pomocniczy.Text = "Aby rozpocząć pomiary połącz się z urządzeniem zgodnym z";
+                    wypisz("Aby rozpocząć pomiary połącz się z urządzeniem zgodnym z");
                 }
             }
             else
             {
                 Polacz.IsEnabled = false;
                 items.Add("Nie znaleziono zadnego urzadzenia");
-                Wynik_pomocniczy.Text = "Pamiętaj by wcześniej sparować urządzenie i włączyć Bluetooth";
+               wypisz("Pamiętaj by wcześniej sparować urządzenie i włączyć Bluetooth");
             }
             Do_polaczenia.ItemsSource = items;
             Do_polaczenia.SelectedIndex = 0;
@@ -109,7 +125,7 @@ namespace WinAlcoFon
             }
             catch (Exception)
             {
-                Wynik_pomocniczy.Text = "Wystąpił błąd podczas wysyłania danych";
+                wypisz("Wystąpił błąd podczas wysyłania danych","!BŁĄD!");
             }
         }
         void blokuj_przyciski(bool zablokowane)
@@ -133,8 +149,10 @@ namespace WinAlcoFon
         }
         void zaladuj_wykres()
         {
+            temp = 0;
+            lista_danych.Clear();
             Random rand = new Random();
-            for(double i=-3.75;i<=0;i+=0.25)
+            for(double i=-5;i<=0;i+=0.25)
             {
                 lista_danych.Add(new dane_do_wykresu() { id = i, wartosc = 0 });
             }
@@ -145,7 +163,8 @@ namespace WinAlcoFon
             Random rand = new Random();
             lista_danych.RemoveAt(lista_danych.IndexOf(lista_danych.First()));
             temp += 0.25;
-            lista_danych.Add(new dane_do_wykresu() { id = temp, wartosc = await pojedynczy_odczyt() });
+            double wart = Math.Round((await pojedynczy_odczyt() - prog_0_promili) * przelicznik, 2);
+            lista_danych.Add(new dane_do_wykresu() { id = temp, wartosc = wart});
             (LineChart.Series[0] as LineSeries).Refresh();
         }
         async void pomiar()
@@ -156,22 +175,20 @@ namespace WinAlcoFon
             wyslij_przez_bt("P");
             for (int i = 0; i < ilosc_probek; i++)
             {
-                Wynik.Text = (ilosc_probek / 4 - i / 4) + " sekund";
+                wypisz("", (ilosc_probek / 4 - i / 4) + " sekund");
                 Progres.Value += (float)100 / ilosc_probek;
                 suma += await pojedynczy_odczyt();
                 await Task.Delay(250);
             }
-            sredni_wynik = suma / ilosc_probek;
-            wynik_w_promilach = Math.Round((sredni_wynik - prog_0_promili) * przelicznik, 3);
-            if (wynik_w_promilach < 0)
-            {
-                wynik_w_promilach = 0;
-            }
+            usredniony_wynik = (double)suma / ilosc_probek;
+            wynik_w_promilach = Math.Round((usredniony_wynik - prog_0_promili) * przelicznik, 3);
             signal.Release();
         }
         private async void Start_Click(object sender, RoutedEventArgs e)
         {
+            zaladuj_wykres();
             blokuj_przyciski(true);
+            wypisz("\nTryb ciągłego odczytu rozpoczęty");
             Stop.IsEnabled = true;
             wyslij_przez_bt("C");
             while (uruchomione)
@@ -182,6 +199,7 @@ namespace WinAlcoFon
         }
         private void Stop_Click(object sender, RoutedEventArgs e)
         {
+            wypisz("\nTryb ciągłego odczytu zakończony");
             blokuj_przyciski(false);
             wyslij_przez_bt("S");
         }
@@ -191,31 +209,40 @@ namespace WinAlcoFon
             //wynik odjac od maksimum jakie moze odczytac wynik i podzielic 
             //pomiary otrzymywane przez BT mnozyc przez ostateczny wynik by otrzymać ‰
             blokuj_przyciski(true);
-            Wynik_pomocniczy.Text = "\nPozostało";
+            wypisz("\nPozostało","");
             pomiar();
             await signal.WaitAsync();
-            Wynik_pomocniczy.Text = "\nCzujnik został skalibrowany";
-            Wynik.Text = "na 0.00‰";
+            wypisz("\nCzujnik został skalibrowany","na 0.00‰");
 
-            prog_0_promili = sredni_wynik;
+            prog_0_promili = usredniony_wynik;
             przelicznik = 8.4 / (4096 - prog_0_promili);
             blokuj_przyciski(false);
-            Kalibruj.Content = "Czujnik skalibrowany";
+            Kalibruj.Content = "Ponowna kalibracja";
         }
         private async void Pomiar_Click(object sender, RoutedEventArgs e)
         {
             blokuj_przyciski(true);
-            Wynik_pomocniczy.Text = "Weź głęboki wdech,\npomiar za ";
             for (int i = 3; i>=1; i--)
             {
-                Wynik.Text = i + " sekund";
+                wypisz("Weź głęboki wdech,\npomiar za ", i + " sekund");
                 await Task.Delay(1000);
             }
-            Wynik_pomocniczy.Text = "\nDmuchaj przez";
+            wypisz("\nDmuchaj przez");
             pomiar();
             await signal.WaitAsync();
-            Wynik_pomocniczy.Text = "\nWynik pomiaru to:";
-            Wynik.Text = wynik_w_promilach+" ‰";
+            if (wynik_w_promilach >= 0)
+            {
+                wypisz("\nWynik pomiaru to:", wynik_w_promilach + " ‰");
+            }
+            else if (wynik_w_promilach < -0.01)
+            {
+                wypisz("Czujnik był niegotowy lub był źle skalibrowany, stąd wynik to:", wynik_w_promilach + " ‰");
+            }
+            else
+            {
+                wypisz("\nWynik pomiaru to:", "0.00 ‰");
+
+            }
             blokuj_przyciski(false);
         }
         private async void Polacz_Click(object sender, RoutedEventArgs e)
@@ -235,7 +262,7 @@ namespace WinAlcoFon
                 Do_polaczenia.IsEnabled = false;
                 Polacz.IsEnabled = false;
                 Odswiez.IsEnabled = false;
-                Wynik_pomocniczy.Text = "Połączenie zostało nawiązane\nTeraz można rozpocząć pomiary z";
+                wypisz("Połączenie zostało nawiązane,\nteraz można rozpocząć pomiary z");
                 WriterData = new DataWriter(_socket.OutputStream);
                 ReaderData = new DataReader(_socket.InputStream);
             }
@@ -245,12 +272,33 @@ namespace WinAlcoFon
                 Do_polaczenia.IsEnabled = true;
                 Polacz.Content = "Połącz";
                 Polacz.IsEnabled = true;
-                Wynik_pomocniczy.Text = "Sprawdź wybrane urządzenie i czy jest ono dostępne";
+                wypisz("Sprawdź wybrane urządzenie i czy jest ono dostępne");
             }
         }
         private void Odswiez_Click(object sender, RoutedEventArgs e)
         {
             szukaj_bt();
+        }
+        private async void image_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            try
+            { 
+                var dialog = new Windows.UI.Popups.MessageDialog(
+                    "Między pomiarami powinna nastąpić przerwa by czujnik mógł się znów \"przyzwyczaić\" do świeżego powietrza. Należy pamiętać, że jest to prosty czujnik i dokładność pomiarów może nie odzwierciedlać rzeczywistości. Wynik zależy nawet od wilgotności i temperatury w jakiej sprzęt jest używany. NIGDY NIE PROWADŹ SAMOCHODU PO SPOŻYCIU ALKOHOLU\n"+
+                    "Dane do debugowania:"+"\n"+
+                    "próg: " + prog_0_promili + '\n' +
+                    "przelicznik: " + przelicznik + '\n'+
+                    "nieprzeliczony ostatni pomiar: "+usredniony_wynik,
+                    "Informacje ogólne i uwagi");//tytuł po przecinku
+                dialog.Commands.Add(new Windows.UI.Popups.UICommand("OK") { Id = 0 });
+
+                if (Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily != "Windows.Mobile")
+                {
+                    //Nie wykona się na Telefonie
+                }
+                var result = await dialog.ShowAsync();
+            }
+            catch(Exception){}
         }
     }
 }
